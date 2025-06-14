b14dcc274e32179d509f7c737ec41368048fa8f6
savelik13 <savelik13@gmail.com>
2025-06-14 18:39:03 +0300

    Update chat.js

--- src/api/chat.js
+++ src/api/chat.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Only POST requests allowed" });
   }
 
-  const apiKey = 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;//process.env.OPENAI_API_KEY;
+  const apiKey = ;//process.env.OPENAI_API_KEY;
 
   try {
     const openaiRes = await fetch("https://api.openai.com/v1/chat/completions", {
